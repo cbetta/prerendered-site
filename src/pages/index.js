a32dac9fe652b67c8fb605275f6c7415b2bb4a5e
Cristiano Betta <cbetta@box.com>
2020-06-10 13:55:41 +0100

    Lazy load a list of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from "react"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
+  const [pages, setPages] = useState([])
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  useEffect(() => {
+    fetch('https://api.github.com/repos/box/developer.box.com/contents/guides?ref=en')
+      .then(response => response.json())
+      .then(data => setPages(data))
+  }, [])
+
+  return  (
+    <>
+      <h1>Pages</h1>
+      { pages[0] && (
+        <ul>
+          { pages.map((page, index) => (
+            <li key={index}>{ page.name }</li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+}
 
 export default IndexPage
